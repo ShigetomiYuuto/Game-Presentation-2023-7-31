6fabda4c5c843483a27061b12ab566a3af9cbda1
Octorok192 <awmcco893@i.softbank.jp>
2023-09-05 19:35:51 +0900

    スコアボードをすぐに切り替えずに、数字がゆっくりと上がるようにした。

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -2,29 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class GameManager : MonoBehaviour
 {
     int _score;
-    [SerializeField] Text _scoreText = null;
+    [SerializeField] Text _scoreText = default;
+    [SerializeField] float _scoreChangeInterval = 0.5f;
 
     void Start()
     {
         AddScore(0);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void AddScore(int score)
     {
-        _score += score;
-        if (_scoreText)
-        {
-            _scoreText.text = "Score: " + _score.ToString("d8");
-        }
+        DOTween.To(() => _score,newValue => 
+        _scoreText.text = "Score:" + newValue.ToString("00000000"),_score + score, _scoreChangeInterval).OnComplete(() => _score += score);
+
+        //_score += score;
+        //if (_scoreText)
+        //{
+        //    _scoreText.text = "Score: " + _score.ToString("d8");
+        //}
     }
 }
