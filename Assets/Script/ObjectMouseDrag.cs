4c84e2b3bc9d86aaec5c48d55b213bfa0967f984
Octorok192 <awmcco893@i.softbank.jp>
2023-09-21 22:24:53 +0900

    オブジェクトをドラッグした際に、オブジェクトの当たり判定を消すようにした。

--- Assets/Script/ObjectMouseDrag.cs
+++ Assets/Script/ObjectMouseDrag.cs
@@ -5,16 +5,19 @@ public class ObjectMouseDrag : MonoBehaviour
     private Vector3 offset;
     private Rigidbody2D _rb;
     private float _gravity = 0f;
+    private Collider2D _collider;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<Collider2D>();
     }
     void OnMouseDown()//マウスでゲームオブジェクトをクリックしたときに呼び出されるメソッド   参考元　https://soft-rime.com/post-11206/
     {
         offset = gameObject.transform.position - GetMouseWorldPos();
         _gravity = _rb.gravityScale;
         _rb.gravityScale = 0;
+        _collider.isTrigger = true;
     }
 
     void OnMouseDrag()//マウスでゲームオブジェクトをドラッグしているときに呼び出されるときに呼ばれるメソッド
@@ -26,6 +29,7 @@ public class ObjectMouseDrag : MonoBehaviour
     {
         transform.position = GetMouseWorldPos() + offset;
         _rb.gravityScale = _gravity;
+        _collider.isTrigger = false;
     }
 
     private Vector3 GetMouseWorldPos()//マウスカーソルの座標をスクリーン座標からワールド座標に変換するメソッド
