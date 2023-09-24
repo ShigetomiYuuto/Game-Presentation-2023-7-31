ce3bd0c3e477651c984e2ec19f303f3c431ef270
Octorok192 <awmcco893@i.softbank.jp>
2023-09-25 01:06:12 +0900

    Qキーを押した際にゲームが一時停止して、タイトルとゲームを終了することができるようにした。

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -10,11 +10,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField] Vector2 _moveInput;
     [SerializeField] float _jumpP;
     [SerializeField] bool _isGrounded = true;
+    [SerializeField] GameObject _gameManager = null;
+    public AudioClip _sound1;
     Rigidbody2D _rb = default;
+    AudioSource _audioSource;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -50,6 +54,23 @@ public class PlayerController : MonoBehaviour
                 _rb.AddForce(Vector2.up * _jumpP, ForceMode2D.Impulse);
                 _isGrounded = false;
             }
+        }
+    }
+    public void OnPause(InputAction.CallbackContext context)
+    {
+        //_audioSource.PlayOneShot(_sound1);
+        if (_gameManager.GetComponent<PausedManager>()._isPaused)
+        {
+            _gameManager.GetComponent<PausedManager>()._isPaused = false;
+            Time.timeScale = 1;
+            Debug.Log("a");
+
+        }
+        else
+        {
+            _gameManager.GetComponent<PausedManager>()._isPaused = true;
+            Time.timeScale = 0;
+            Debug.Log("b");
         }
     }
 }
